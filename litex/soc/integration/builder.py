be0d4b84154f1feb7d75d6918c50be4acf4e32fc
Mateusz Bieganski <mateusz.bieganski@plumerai.com>
2022-05-10 19:55:27 +0200

    fix previous

--- litex/soc/integration/builder.py
+++ litex/soc/integration/builder.py
@@ -285,7 +285,7 @@ class Builder:
         # Get BIOS data from compiled BIOS binary.
         bios_file = os.path.join(self.software_dir, "bios", "bios.bin")
         os.system(f"rm -rf {bios_file}")
-        os.system(f"riscv-none-embed-objcopy -O binary /home/mateusz/Ikva/sw/ikva_lce/build.ikva_elf.elf {bios_file}")
+        os.system(f"riscv-none-embed-objcopy -O binary /home/mateusz/Ikva/sw/ikva_lce/build/ikva_elf.elf {bios_file}")
         bios_data = soc_core.get_mem_data(bios_file, self.soc.cpu.endianness)
 
         # Initialize SoC with with BIOS data.
